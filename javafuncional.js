568d9bbd3f6d983d78d6ad87a197408b953e2088
chamoninho <135061463+chamoninho@users.noreply.github.com>
2023-05-31 21:14:29 -0300

    Delete javafuncional.js

--- javafuncional.js
+++
@@ -1,9 +0,0 @@
-function register(action) {
-  var statusDiv = document.getElementById("status");
-
-  if (action === "Entrou") {
-    statusDiv.textContent = "Alguém está usando o Timeline.";
-  } else if (action === "Saiu") {
-    statusDiv.textContent = "Ninguém está usando o Timeline.";
-  }
-}
